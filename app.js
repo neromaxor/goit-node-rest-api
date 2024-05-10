cb8e6b57ddb9d85fbe1b479df371f1b22e33f463
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-05-11 00:51:09 +0300

    update

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+import contactsRouter from "./routes/contactsRouter.js";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
 
-import contactsRouter from "./routes/contactsRouter.js";
+const DB_URI =
+  "mongodb+srv://student-3:32$teU5HeAcX_*A@cluster0.2sd7feu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+async function run() {
+  try {
+    await mongoose.connect(DB_URI);
+    console.info("Database connection successfully");
+  } catch (error) {
+    console.error("Database connection error:", error);
+    process.exit(1); // Припиняє виконання програми з кодом помилки
+  }
+}
+
+run().catch((error) => console.error());
 
 const app = express();
 
--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,70 @@
-import contactsService from "../services/contactsServices.js";
+import ContactModel from "../contactModel/contactModel.js"; // Додайте імпорт моделі
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await ContactModel.find(); // Використовуйте модель для отримання контактів
+    res.status(200).json(contacts);
+  } catch (error) {
+    console.error("Error getting all contacts:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const contact = await ContactModel.findById(id); // Використовуйте модель для отримання контакту за id
+    if (contact) {
+      res.status(200).json(contact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    console.error("Error fetching contact by id:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const contact = await ContactModel.findByIdAndRemove(id); // Використовуйте модель для видалення контакту за id
+    if (contact) {
+      res.status(200).json(contact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    console.error("Error deleting contact:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {
+  try {
+    const { name, email, phone } = req.body;
+    const newContact = await ContactModel.create({ name, email, phone }); // Використовуйте модель для створення нового контакту
+    res.status(201).json(newContact);
+  } catch (error) {
+    console.error("Error creating contact:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const newData = req.body;
+    const updatedContact = await ContactModel.findByIdAndUpdate(id, newData, {
+      new: true,
+    }); // Використовуйте модель для оновлення контакту за id
+    if (updatedContact) {
+      res.status(200).json(updatedContact);
+    } else {
+      res.status(404).json({ message: "Contact not found" });
+    }
+  } catch (error) {
+    console.error("Error updating contact:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
