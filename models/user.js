ec4ddc6d22d32ade19dac61989c5032031b5407b
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-05-31 00:24:38 +0300

    update

--- models/user.js
+++ models/user.js
@@ -11,7 +11,11 @@ const userSchema = new mongoose.Schema(
       required: [true, "Email is required"],
       unique: true,
     },
-
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
     token: {
       type: String,
       default: null,
